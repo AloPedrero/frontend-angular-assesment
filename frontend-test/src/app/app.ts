18059432716da25cbc2e573d8300c8e3d6a0cc5d
AloPedrero <pedreromartinezalonso@gmail.com>
2026-05-22 13:09:01 -0600

    feat: created base app layout

--- frontend-test/src/app/app.ts
+++ frontend-test/src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { BookingList } from './components/booking-list/booking-list';
+import { BookingDetail } from './components/booking-detail/booking-detail';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    BookingList,
+    BookingDetail
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend-test');
+
 }
