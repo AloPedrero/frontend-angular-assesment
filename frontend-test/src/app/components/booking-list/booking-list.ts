832993590c8daa1a90e526170cb8eb435f19713c
AloPedrero <pedreromartinezalonso@gmail.com>
2026-05-22 13:30:41 -0600

    feat: created dynamic card classes

--- frontend-test/src/app/components/booking-list/booking-list.ts
+++ frontend-test/src/app/components/booking-list/booking-list.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookingService } from '../../services/booking';
+import { Booking } from '../../models/booking.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-booking-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './booking-list.html',
   styleUrl: './booking-list.scss',
 })
-export class BookingList {}
+
+export class BookingList implements OnInit {
+
+  bookings: Booking[] = [];
+
+  constructor(private bookingService: BookingService) {}
+
+  ngOnInit(): void {
+    this.bookingService.getBookings().subscribe(data => {
+      this.bookings = data;
+    });
+  }
+
+}
